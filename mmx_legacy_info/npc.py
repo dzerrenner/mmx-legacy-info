e0d8699132fec7a25f575d3555937b7fe46530cd
dzerrenner <dazer017@gmail.com>
2014-03-09 15:58:18 +0100

    added stub for get_dialog()

--- mmx_legacy_info/npc.py
+++ mmx_legacy_info/npc.py
@@ -7,12 +7,15 @@ NPCRecord = namedtuple('NPCRecord', 'id, name_key, hireling_profession, descript
 
 class NPC(NPCRecord):
     """
-    NPC data
+    NPC data.
+
+    The constructor of this class accepts 13 positional arguments in the same order as they are listed in the data file.
+    The data file for generating these lines can be read as follows:
 
     >>> import mmx_legacy_info, os.path, csv
     >>> npc_file = os.path.join(mmx_legacy_info.BASE_DIR, "StaticData", "NpcStaticData.csv")
     >>> reader = csv.reader(open(npc_file, newline=''))
-    >>> npc_list = [NPC(*l) for l in reader]
+    >>> npc_list = [NPC(*line) for line in reader]  # note list expansion
     >>> lord_haart = npc_list[3]
     >>> lord_haart.name_key
     'NPC_NAME_LORD_HAART'
@@ -25,6 +28,12 @@ class NPC(NPCRecord):
 
     >>> lord_haart.is_hireling
     False
+
+    >>> # not yet implemented
+    >>> lord_haart.get_dialog()
+    Traceback (most recent call last):
+    ...
+    NotImplementedError: Dialog extraction is not implemented yet.
 
     """
 
@@ -39,3 +48,14 @@ class NPC(NPCRecord):
     @property
     def has_dialog(self):
         return self.dialog_key != ''
+
+    def get_dialog(self):
+        """
+        Gets the dialog object for this NPC.
+
+        If the NPC has no dialog, a KeyError is raised. So far, every NPC seems to have a dialog attached, but that
+        could be a subject to change.
+        """
+        if not self.has_dialog:
+            raise KeyError("No dialog attached to this NPC.")
+        raise NotImplementedError("Dialog extraction is not implemented yet.")
